d54be7e3499acae25e1c3b322fd397caecc461c8
Troels Abrahamsen <troels@blck.dk>
2014-05-23 15:08:16 +0200

    Updates to the WSS support module

--- support/rigWP_support_woo_subshops.class.php
+++ support/rigWP_support_woo_subshops.class.php
@@ -9,7 +9,7 @@ class rigWP_support_woo_subshops {
 	 * @return void
 	 **/
 	function init(){
-		add_filter('woo_subshops/locate_template_dirs', array($this, 'add_template_dir'), 999, 2);
+		add_filter('wss/locate_template_dirs', array($this, 'add_template_dir'), 999, 2);
 	}
 
 	/**
@@ -17,19 +17,23 @@ class rigWP_support_woo_subshops {
 	 *
 	 * @return void
 	 **/
-	function add_template_dir($dirs, $shop){
+	function add_template_dir($dirs){
 
 		global $rigWP;
 
+		$newdirs = array();
+
+		if($shop = wss::get_current_shop()){
+			$newdirs[] = get_stylesheet_directory().'/'.$rigWP->templates_folder.'/subshops/'.$shop->post_name.'/';
+			$newdirs[] = get_stylesheet_directory().'/'.$rigWP->templates_folder.'/subshops/';
+			$newdirs[] = get_template_directory().'/'.$rigWP->templates_folder.'/subshops/'.$shop->post_name.'/';
+			$newdirs[] = get_template_directory().'/'.$rigWP->templates_folder.'/subshops/';
+		}
+
+		$newdirs[] = get_stylesheet_directory().'/'.$rigWP->templates_folder.'/';
+		$newdirs[] = get_template_directory().'/'.$rigWP->templates_folder.'/';
+
 		/* Append rigWP structure to the dirs */
-		$newdirs = array(
-			get_stylesheet_directory().'/'.$rigWP->templates_folder.'/subshops/'.$shop->post_name.'/',
-			get_stylesheet_directory().'/'.$rigWP->templates_folder.'/subshops/',
-			get_template_directory().'/'.$rigWP->templates_folder.'/subshops/'.$shop->post_name.'/',
-			get_template_directory().'/'.$rigWP->templates_folder.'/subshops/',
-			get_stylesheet_directory().'/'.$rigWP->templates_folder.'/',
-			get_template_directory().'/'.$rigWP->templates_folder.'/'
-			);
 		$dirs = $newdirs+$dirs;
 
 		return $dirs;
